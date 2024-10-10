b94738b06167e95b0fc122228d3cb52cd5f5c489
Yugsingh05 <yugsingh7622@gmail.com>
2024-10-10 13:29:07 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import dotenv from "dotenv"
-import ConnectDB from "./db/index.js"
+import dotenv from 'dotenv';
+import ConnectDB from './db/index.js';
+import {app} from './app.js';
 
+dotenv.config ({
+  path: './env',
+});
 
-dotenv.config({
-    path : './env'
-})
+ConnectDB ()
+  .then (() => {
+    const PORT = process.env.PORT;
 
-ConnectDB()
+    app.on ('error', error => {
+      console.log ('Error Occured before app', error);
+      throw error;
+    });
+
+    app.listen (PORT, () => {
+      console.log (` Server is listening at ${PORT}`);
+    });
+  })
+  .catch (error => {
+    console.log ('Error at express', error);
+  });
